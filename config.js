441249ddf704c36130fe9bc9b8307ae9876e3185
mantouchoufengle <80181193+mantouchoufengle@users.noreply.github.com>
2021-03-06 19:20:53 -0500

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "心爱的肉肉": "./imgs/xiaokeai.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
@@ -31,7 +31,7 @@ var config = {
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        wish_message: "纪念日快乐",
         story: "A MESSAGE FOR YOU",
     }
 };
